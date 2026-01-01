d5179746b6ec45644398b7898e0a4397b9e54ef1
Daniel Mantz <st172086@stud.uni-stuttgart.de>
2026-01-01 20:39:49 +0100

    added products to database

--- products.js
+++ products.js
@@ -1,86 +1,173 @@
 const products = [
 {
     id: 1,
-    title: "I will wait here for you",
-    category: "Print",
-    dimensions: "Dimensions: 28,4 x 21,0",
-    price: "10,00€",
-    description: "This print has been created through unethical labor practices involving children.",
-    mainImage: "img/gallery-1.png",
+    title: "Modern Torture",
+    category: "Original Painting",
+    dimensions: "Dimensions: 60,0 cm x 80,0 cm",
+    price: "2300,00€",
+    description: "In dem Bild gehts um Arbeit.",
+    mainImage: "img/product-img/001-img-01.png",
     thumbnails: [
-        "img/gallery-2.png",
-        "img/gallery-3.png",
-        "img/gallery-4.png"
     ]
 },
 {
     id: 2,
-    title: "Game of Life",
-    category: "Print",
-    dimensions: "Dimensions: 1000,0x10000,0",
-    price: "10,00€",
-    description: "This print has been created through unethical labor practices involving children.",
-    mainImage: "img/gallery-1.png",
+    title: "Way of Life",
+    category: "Original Painting",
+    dimensions: "Dimensions: 60,0 cm x 90,0 cm",
+    price: "2500,00€",
+    description: "Relibuch.",
+    mainImage: "img/product-img/002-img-01.png",
     thumbnails: [
-        "img/gallery-2.png",
-        "img/gallery-3.png",
-        "img/gallery-4.png"
     ]
 },
 {
     id: 3,
-    title: "Contemporary Torment",
-    category: "Artwork for License",
-    dimensions: "Dimensions: 1000,0x10000,0",
-    price: "1.000.000,00€",
-    description: "This artowrk has been created through unethical labor practices involving children.",
-    mainImage: "img/gallery-1.png",
+    title: "Way of Life (Poster)",
+    category: "Poster",
+    dimensions: "Dimensions: 29,7 cm x 42,0 cm (DIN A3)",
+    price: "10,00€",
+    description: "Relibuch Poster.",
+    mainImage: "img/product-img/003-img-01.png",
     thumbnails: [
-        "img/gallery-2.png",
-        "img/gallery-3.png",
-        "img/gallery-4.png"
     ]
 },
 {
     id: 4,
-    title: "Jeany ist am Ende",
-    category: "Print",
-    dimensions: "Dimensions: 1,0x1,0",
-    price: "2,00€",
-    description: "This print has been created through unethical labor practices involving children.",
-    mainImage: "img/gallery-1.png",
+    title: "Anxiet.Y",
+    category: "Original Painting",
+    dimensions: "Dimensions: 70,0 cm x 89.5 cm",
+    price: "1900,00€",
+    description: "Hedvig reference.",
+    mainImage: "img/product-img/004-img-01.png",
     thumbnails: [
-        "img/gallery-2.png",
-        "img/gallery-3.png",
-        "img/gallery-4.png"
     ]
 },
 {
     id: 5,
-    title: "Rot-Rot-Grün",
-    category: "Print",
-    dimensions: "Dimensions: 13,0x12,0",
-    price: "7,00€",
-    description: "This print has been created through unethical labor practices involving children.",
-    mainImage: "img/gallery-1.png",
+    title: "The Human Experience",
+    category: "Original Painting",
+    dimensions: "Dimensions: 70,0 cm x 90,0 cm",
+    price: "3000,00€",
+    description: "Kiffen ist geil bis du dich so fühlst.",
+    mainImage: "img/product-img/005-img-01.png",
     thumbnails: [
-        "img/gallery-2.png",
-        "img/gallery-3.png",
-        "img/gallery-4.png"
     ]
 },
 {
     id: 6,
-    title: "Dababy",
-    category: "Original",
-    dimensions: "Dimensions: 15,0x3,0",
-    price: "3.000,00€",
-    description: "This painting has been created through unethical labor practices involving children.",
-    mainImage: "img/gallery-1.png",
+    title: "Emotions",
+    category: "Original Painting",
+    dimensions: "Dimensions: 30,0 cm x 30,0 cm",
+    price: "500,00€",
+    description: "thealwayspresentend.mp3",
+    mainImage: "img/product-img/006-img-01.png",
     thumbnails: [
-        "img/gallery-2.png",
-        "img/gallery-3.png",
-        "img/gallery-4.png"
+    ]
+},
+{
+    id: 7,
+    title: "On War (Series of 3 Paintings)",
+    category: "Original Painting",
+    dimensions: "Dimensions: 24,0 cm x 30,0 cm (3x)",
+    price: "600,00€",
+    description: "Die gesamte Serie.",
+    mainImage: "img/product-img/007-img-01.png",
+    thumbnails: [
+        "img/product-img/007-img-02.png",
+        "img/product-img/007-img-03.png",
+        "img/product-img/007-img-04.png",
+    ]
+},
+{
+    id: 8,
+    title: "Baby Soldier",
+    category: "Original Painting",
+    dimensions: "Dimensions: 24,0 cm x 30,0 cm",
+    price: "200,00€",
+    description: "Verstörend.",
+    mainImage: "img/product-img/008-img-01.png",
+    thumbnails: [
+    ]
+},
+{
+    id: 9,
+    title: "How long is Peace?",
+    category: "Original Painting",
+    dimensions: "Dimensions: 24,0 cm x 30,0 cm",
+    price: "300,00€",
+    description: "Fuck that one goldfish in particular.",
+    mainImage: "img/product-img/009-img-01.png",
+    thumbnails: [
+    ]
+},
+{
+    id: 10,
+    title: "Greedy Baby",
+    category: "Original Painting",
+    dimensions: "Dimensions: 24,0 cm x 30,0 cm",
+    price: "250,00€",
+    description: "Deutsche Nirvana.",
+    mainImage: "img/product-img/010-img-01.png",
+    thumbnails: [
+    ]
+},
+{
+    id: 11,
+    title: "Ihr quält mich immer noch",
+    category: "Original Painting",
+    dimensions: "Dimensions: 70,0 cm x 90,0 cm",
+    price: "4000,00€",
+    description: "Compressed screenshot placeholder.",
+    mainImage: "img/product-img/011-img-01.png",
+    thumbnails: [
+    ]
+},
+{
+    id: 12,
+    title: "I will wait here for you",
+    category: "Lino Print",
+    dimensions: "Dimensions: 21,0 cm x 28,4 cm",
+    price: "15,00€",
+    description: "Frame or guy not included.",
+    mainImage: "img/product-img/012-img-01.png",
+    thumbnails: [
+        "img/product-img/012-img-02.png",
+        "img/product-img/012-img-03.png",
+        "img/product-img/012-img-04.png",
+    ]
+},
+{
+    id: 13,
+    title: "Shroom",
+    category: "Original Painting",
+    dimensions: "Dimensions: 14,8 cm x 21,0 cm (DIN A5)",
+    price: "60,00€",
+    description: "Painting looks sour cream flavored.",
+    mainImage: "img/product-img/013-img-01.png",
+    thumbnails: [
+    ]
+},
+{
+    id: 14,
+    title: "Pond Activity",
+    category: "Original Painting",
+    dimensions: "Dimensions: 21,0 cm x 14,8 cm (DIN A5)",
+    price: "40,00€",
+    description: "Aka Frog Business.",
+    mainImage: "img/product-img/014-img-01.png",
+    thumbnails: [
+    ]
+},
+{
+    id: 15,
+    title: "Fired Up",
+    category: "Original Painting",
+    dimensions: "Dimensions: 21,0 cm x 14,8 cm (DIN A5)",
+    price: "50,00€",
+    description: "Petrol Girls 2",
+    mainImage: "img/product-img/015-img-01.png",
+    thumbnails: [
     ]
 }
 ];
